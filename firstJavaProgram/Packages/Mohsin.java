6449d37fbaf7e4e1faf2724b6a51732cc7eb76e2
Asad <asadsamoon8@gmail.com>
2022-08-02 23:47:22 +0500

    New document added

--- firstJavaProgram/Packages/Mohsin.java
+++
@@ -1,9 +0,0 @@
-package Packages;
-
-public class Mohsin{
-    public static void main(String[] args){
-        System.out.println("I am class Mohsin's main method!");
-        // Packages --> Scanner is class in java.util Package
-        //              ArrayList is class in java.util Package
-    }     // end of main() method
-}        //  end of program
--- firstJavaProgram/Packages/Packages.java
+++
@@ -1,9 +0,0 @@
-package Packages;
-
-public class Packages{
-    public static void main(String[] args){
-        System.out.println("I am class Packages's main method!");
-        // Packages --> Scanner is class in java.util Package
-        //              ArrayList is class in java.util Package
-    }     // end of main() method
-}        //  end of program
